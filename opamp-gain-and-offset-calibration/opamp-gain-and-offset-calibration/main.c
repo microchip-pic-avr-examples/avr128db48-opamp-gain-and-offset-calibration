0d1fddeec1649a43441c80a01fa341d666351b34
Carl Einar Hellenes <CarlEinar.Hellenes@Microchip.com>
2020-06-30 11:52:52 +0200

    MPAE-4226: Changed opamp definitions in code to math DFP version 1.2.49. Added copyright text at top of code

--- opamp-gain-and-offset-calibration/opamp-gain-and-offset-calibration/main.c
+++ opamp-gain-and-offset-calibration/opamp-gain-and-offset-calibration/main.c
@@ -1,3 +1,28 @@
+/*
+ * \file main.c
+ *
+ * \brief avr128db48-opamp-gain-and-offset-calibration
+ *
+ *  (c) 2020 Microchip Technology Inc. and its subsidiaries.
+ *
+ *  Subject to your compliance with these terms,you may use this software and
+ *  any derivatives exclusively with Microchip products.It is your responsibility
+ *  to comply with third party license terms applicable to your use of third party
+ *  software (including open source software) that may accompany Microchip software.
+ *
+ *  THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+ *  EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+ *  WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+ *  PARTICULAR PURPOSE.
+ *
+ *  IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+ *  INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+ *  WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+ *  BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+ *  FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+ *  ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ *  THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ */
 #define F_CPU 4000000UL // 4 MHz is default CPU and peripheral frequency
 
 #include <avr/io.h>
@@ -28,8 +53,8 @@
 #define N_AVERAGE 100
 
 void measure(uint16_t dac_data, uint8_t muxpos_dacout, uint8_t muxpos_opout,
-				int16_t *dac_meas, int16_t *opout_meas){
-					
+int16_t *dac_meas, int16_t *opout_meas){
+	
 	//Given a DAC setting, this function measures the op amp input and output values
 	
 	DAC0.DATA = dac_data << DAC_DATA0_bp; // Write a new value to the DAC
@@ -49,11 +74,11 @@ void measure(uint16_t dac_data, uint8_t muxpos_dacout, uint8_t muxpos_opout,
 	
 	return;
 }
-	
+
 volatile float meas_gain = 0; // Measured gain
 volatile float meas_offset = 0; // Measured input offset in ADC units
 volatile float meas_offset_mv = 0; // Measured input offset in mV
-	
+
 int main(void)
 {
 	uint8_t n;
@@ -68,15 +93,15 @@ int main(void)
 	
 	// Disable digital input on DAC output pin
 	PORTD.PIN6CTRL = PORT_ISC_INPUT_DISABLE_gc;
-    
+	
 	// Set up the timebase of the OPAMP peripheral
 	OPAMP.TIMEBASE = 3; // Number of CLK_PER cycles that equal one us, minus one (4-1=3)
 
 	//Configure OP0 as non-inverting gain of 16 with DAC as input
-	OPAMP.OP0INMUX = OPAMP_MUXPOS_DAC_gc | OPAMP_MUXNEG_WIP_gc;
-	OPAMP.OP0RESMUX = OPAMP_MUXBOT_GND_gc | OPAMP_MUXWIP_WIP7_gc | OPAMP_MUXTOP_OUT_gc;
+	OPAMP.OP0INMUX = OPAMP_OP0INMUX_MUXPOS_DAC_gc | OPAMP_OP0INMUX_MUXNEG_WIP_gc;
+	OPAMP.OP0RESMUX = OPAMP_OP0RESMUX_MUXBOT_GND_gc | OPAMP_OP0RESMUX_MUXWIP_WIP7_gc | OPAMP_OP0RESMUX_MUXTOP_OUT_gc;
 	// Configure OP0 Control A
-	OPAMP.OP0CTRLA = OPAMP_RUNSTBY_bm |  OPAMP_OUTMODE_NORMAL_gc | OPAMP_ALWAYSON_bm;
+	OPAMP.OP0CTRLA = OPAMP_RUNSTBY_bm |  OPAMP_OP0CTRLA_OUTMODE_NORMAL_gc | OPAMP_ALWAYSON_bm;
 
 	// Enable the OPAMP peripheral
 	OPAMP.CTRLA = OPAMP_ENABLE_bm;
@@ -89,8 +114,8 @@ int main(void)
 	DAC0.CTRLA = DAC_OUTEN_bm | DAC_ENABLE_bm;
 	
 	// Set up ADC peripheral
-	ADC0.CTRLC = ADC_PRESC_DIV4_gc; // Set up ADC prescaler to DIV4 
-	                                // so CLK_ADC = 4 MHz / 4 = 1 MHz
+	ADC0.CTRLC = ADC_PRESC_DIV4_gc; // Set up ADC prescaler to DIV4
+	// so CLK_ADC = 4 MHz / 4 = 1 MHz
 	ADC0.CTRLA = ADC_ENABLE_bm; // Enable ADC in single-ended mode
 	
 	accum_xa = 0; accum_ya = 0; accum_xb = 0; accum_yb = 0; //Reset accumulators
@@ -105,14 +130,14 @@ int main(void)
 	avg_ya = ((float) accum_ya)/((float) N_AVERAGE);
 	avg_xb = ((float) accum_xb)/((float) N_AVERAGE);
 	avg_yb = ((float) accum_yb)/((float) N_AVERAGE);
-		
+	
 	meas_gain = (avg_yb - avg_ya)/(avg_xb - avg_xa); // Measured gain
 	meas_offset = (avg_ya/meas_gain) - avg_xa; // Measured input offset in ADC units
 	meas_offset_mv = meas_offset * MV_PER_ADC_LSB; // Measured input offset in mV
 	
 	// Calibration is complete and the OPAMP is ready to be used by the application,
 	// so configure the OPAMP input mux to select the input pin.
-	OPAMP.OP0INMUX = OPAMP_MUXPOS_INP_gc | OPAMP_MUXNEG_WIP_gc;
+	OPAMP.OP0INMUX = OPAMP_OP0INMUX_MUXPOS_INP_gc | OPAMP_OP0INMUX_MUXNEG_WIP_gc;
 	
 	while(1){
 		// Place application here
